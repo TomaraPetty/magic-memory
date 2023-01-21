be37ce29b07c88493a9e797fee6f09e81b25b693
Tomara Petty <tomararuth@gmail.com>
2023-01-21 13:51:24 -0800

    start game automatically

--- src/App.js
+++ src/App.js
@@ -32,6 +32,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map(card => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -69,6 +71,11 @@ function App() {
     setDisabled(false)
   };
 
+  // Start automatically 
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className='App'>
       <h1>Technical Skills</h1>
@@ -85,6 +92,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
